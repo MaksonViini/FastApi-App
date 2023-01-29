1a4180dcbc79d92749073297f8a647ef5885e5ad
Makson Vinicio <maksonvinicio7@gmail.com>
2023-01-29 18:41:38 -0300

    Adicionando mongo async e makefile

--- src/database.py
+++ src/database.py
@@ -1,3 +1,4 @@
+import motor.motor_asyncio
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -10,6 +11,12 @@ settings = Settings(config={'env': 'dev'}).config
 SQLALCHEMY_DATABASE_URL = f"""postgresql://postgres:{settings.db_password}@{settings.db_host}:
                                                         {settings.db_port}/{settings.db_database}"""
 
+DATABASE_URL = f"""mongodb://root:{settings.db_password}@{settings.db_host}:27017/"""
+
+client = motor.motor_asyncio.AsyncIOMotorClient(DATABASE_URL)
+
+database = client['data']
+collection = database['data_health']
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
